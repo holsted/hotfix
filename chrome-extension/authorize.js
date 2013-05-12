9a8105649daf30c65fc0fc6e42d61eb88903150b
FajitaNachos <andrew@fajitanachos.com>
2013-05-12 21:12:40 +0900

    Update welcome page.

--- chrome-extension/authorize.js
+++ chrome-extension/authorize.js
@@ -38,9 +38,8 @@
             // Add the username to the data object.
 
             data.username = user.login;
-            chrome.extension.sendMessage({greeting: "reload_background", data: data}, function(response){
-                callback();
-            });
+            chrome.extension.sendMessage({greeting: "reload_background", data: data}, function(response){ });
+            callback();
         });     
     });
 
